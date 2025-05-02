02e1bc93eb170f2bd6ff2e652504398ff29fa126
kavinda Methsara <methsara31@gmail.com>
2025-05-02 07:54:32 +0000

    Automated opening the browser and quit the browser.

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -8,12 +8,17 @@ import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class Main {
 
+  static protected WebDriver driver;/*Declaring a WebDriver Object*/
 
   static {
-
+    chromedriver().setup();/*ChromeDriver executable-Automatically download and configure the browser*/
+    var options = new ChromeOptions();/*To change the brower behavior */
+    options.addArguments("--no-sandbox");
+    driver = new ChromeDriver(options);
   }
 
   public static void main(String[] args) {
-
+    driver.get("https://google.com");
+    driver.quit();
   }
 }
