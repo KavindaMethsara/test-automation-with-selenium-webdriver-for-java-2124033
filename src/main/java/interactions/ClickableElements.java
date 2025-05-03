f6b7c029435feda8e32ef9a99c48ecab30937a03
kavinda Methsara <methsara31@gmail.com>
2025-05-03 09:02:30 +0000

    WebDriver to click web elements such as buttons and links

--- src/main/java/interactions/ClickableElements.java
+++ src/main/java/interactions/ClickableElements.java
@@ -8,8 +8,15 @@ public class ClickableElements extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    // WebDriver to click web elements such as buttons and links
 
-    
+    //Submit button
+    driver.findElement(By.xpath("//button[text()='Submit']")
+    ).click();
+
+    //Hyperlink
+    driver.findElement(By.xpath("//a[normalize-space(text())='Return to index']")
+    ).click();
 
     driver.quit();
   }
