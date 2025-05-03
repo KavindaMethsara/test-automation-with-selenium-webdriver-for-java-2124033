bb1202e2c1f1ec4879cb94cd28676b609cd27bbc
kavinda Methsara <methsara31@gmail.com>
2025-05-03 08:17:36 +0000

    Text Boxes

--- src/main/java/interactions/Textboxes.java
+++ src/main/java/interactions/Textboxes.java
@@ -10,8 +10,11 @@ public class Textboxes extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
-    
+    WebElement textbox = driver.findElement(By.name("id-name1"));
+    textbox.getText();//This will return the string of the text in the text box
+    textbox.clear();//To remove the text in the text box
+    textbox.sendKeys("Sample text-01");
 
-    driver.quit();
+    //driver.quit();
   }
 }
