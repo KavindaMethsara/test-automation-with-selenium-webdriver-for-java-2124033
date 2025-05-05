d417aa499e15c36f509eed033a452f0d23bd850f
kavinda Methsara <methsara31@gmail.com>
2025-05-05 09:41:39 +0000

    Manage the implicit wait for interacting a element

--- src/main/java/wait_strategies/ImplicitWaits.java
+++ src/main/java/wait_strategies/ImplicitWaits.java
@@ -8,7 +8,7 @@ public class ImplicitWaits extends Main {
 
   public static void main(String[] args) {
     
-    
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));//Manage the implicit wait for interacting a element
     driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
     driver.findElement(By.id("reveal")).click();
     driver.findElement(By.id("revealed")).sendKeys("I see you");
