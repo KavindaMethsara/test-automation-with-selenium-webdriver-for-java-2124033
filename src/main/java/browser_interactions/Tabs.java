c80005a3d4fb3f0d1d736c2481f0ccc91c36cb09
kavinda Methsara <methsara31@gmail.com>
2025-05-07 09:10:38 +0000

    Tabs

--- src/main/java/browser_interactions/Tabs.java
+++ src/main/java/browser_interactions/Tabs.java
@@ -10,7 +10,17 @@ public class Tabs extends Main {
     driver.get("https://www.selenium.dev/selenium/web/window_switching_tests/page_with_frame.html");
 
     driver.findElement(By.id("a-link-that-opens-a-new-window")).click();
+    var windows = driver.getWindowHandles();//To get unique identifiers for windows or tabs that is currently open within this session
+    System.out.println(windows);
 
+    String originalWindow = driver.getWindowHandle();
+    for(String window : windows){
+      if(!window.equals(originalWindow)){
+        driver.switchTo().window(window);
+      }
+    }
+
+    System.out.println(driver.getTitle());
 
     driver.quit();
   }
