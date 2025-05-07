acdd4db5d1fe2096abf5243f7d8a0cdab85771bb
kavinda Methsara <methsara31@gmail.com>
2025-05-07 08:40:13 +0000

    Browser Info

--- src/main/java/browser_interactions/BrowserInfo.java
+++ src/main/java/browser_interactions/BrowserInfo.java
@@ -7,7 +7,11 @@ public class BrowserInfo extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+    String title = driver.getTitle();//To get the Title of the web page
+    String url = driver.getCurrentUrl();//To get the url of the page
 
+    System.out.println(title);
+    System.out.println(url);
 
     driver.quit();
   }
