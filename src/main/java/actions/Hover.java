0e8470e39d71b3cf9ba2210a29179ed0b12cb17c
kavinda Methsara <methsara31@gmail.com>
2025-05-07 17:03:06 +0000

    Hover

--- src/main/java/actions/Hover.java
+++ src/main/java/actions/Hover.java
@@ -10,8 +10,12 @@ public class Hover extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/mouseOver.html");
 
+    var redBox = driver.findElement(By.id("redbox"));
+    System.out.println(redBox.getCssValue("background-color"));
 
-
+    new Actions(driver).moveToElement(redBox).perform();
+    System.out.println(redBox.getCssValue("background-color"));
+    
     driver.quit();
   }
 }
